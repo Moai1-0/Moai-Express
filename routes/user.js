089699985510a01b2e022d5e3a5241ebaa6c4673
JungTag <dbsdudxor121@naver.com>
2021-07-29 16:02:13 +0900

    feat: 상품 상세 조회 API 추가

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const user = require('../controllers/user');
 
 router.get('/products', user.getProducts);
+router.get('/product', user.getProduct);
 
 module.exports = router;
