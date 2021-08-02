1bccd3f082697b58f589d4f22f141f2057303a59
JungTag <dbsdudxor121@naver.com>
2021-08-03 02:08:10 +0900

    feat: 상품 예약 추가

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const user = require('../controllers/user');
 
 router.get('/products', user.getProducts); // 상품 리스트 조회
 router.get('/product', user.getProduct); // 상품 상세 조회
+router.post('/reserve', user.reserveProduct); // 상품 예약
 router.post('/confirm', user.confirmUser); // 구매내역 - 로그인
 
 
