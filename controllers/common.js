0f9db6654bda98e7996e269965aa0845104344a5
JungTag <dbsdudxor121@naver.com>
2021-08-02 17:45:19 +0900

    feat: 소비자 로그인 추가

--- controllers/common.js
+++ controllers/common.js
@@ -1,6 +1,13 @@
 const err = require('http-errors');
 
 const controller = {
+    async main (req, res, next) {
+        try {
+            next({ message: "main" });
+        } catch (e) {
+            next(e);
+        }
+    },
     async ping (req, res, next) {
         try {
             next({ message: "ping" });
--- routes/common.js
+++ routes/common.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const common = require('../controllers/common');
 
+router.get('/', common.main);
 router.get('/ping', common.ping);
 
 module.exports = router;
