e35fa89d8f0d6fcd1fee01234a41054d98184066
JungTag <dbsdudxor121@naver.com>
2021-08-09 15:26:57 +0900

    feat: 환불 계좌 조회 및 수정 API 추가

--- routes/user.js
+++ routes/user.js
@@ -14,5 +14,7 @@ router.get('/history/ongoing', checkUser, user.getReservationHistory); // 구매
 router.get('/history/done', checkUser, user.getPurchaseHistory); // 구매내역 - 수령/환급
 router.get('/point', checkUser, user.getRemainingPoint); // 잔여 포인트 조회
 router.post('/return', checkUser, user.applyForReturn); // 환급 신청
+router.get('/account', checkUser, user.getReturnAccount); // 환불 계좌 조회
+router.patch('/account', checkUser, user.editReturnAccount); // 환불 계좌 수정
 
 module.exports = router;
