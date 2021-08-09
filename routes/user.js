7a3ce1faa02d8fad3b1e68ba528f7669ca831a56
JungTag <dbsdudxor121@naver.com>
2021-08-09 13:10:38 +0900

    feat: 환급 신청 API 추가

--- routes/user.js
+++ routes/user.js
@@ -10,8 +10,9 @@ router.post('/signup', user.signup); // 회원가입
 router.post('/signin', user.signin); // 로그인
 router.post('/reserve', checkUser, user.reserveProduct); // 상품 예약
 router.patch('/reserve', checkUser, user.cancelReservation); // 상품 예약 취소
-router.get('/history/ongoing', checkUser, user.getReservationHistory) // 구매내역 - 진행중
-router.get('/history/done', checkUser, user.getPurchaseHistory) // 구매내역 - 수령/환급
-router.get('/point', checkUser, user.getRemainingPoint) // 잔여 포인트 조회
+router.get('/history/ongoing', checkUser, user.getReservationHistory); // 구매내역 - 진행중
+router.get('/history/done', checkUser, user.getPurchaseHistory); // 구매내역 - 수령/환급
+router.get('/point', checkUser, user.getRemainingPoint); // 잔여 포인트 조회
+router.post('/return', checkUser, user.applyForReturn); // 환급 신청
 
 module.exports = router;
