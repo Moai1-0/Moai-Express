6d2a0f8e8a9e603c625a4edb2cbacd6c2e24067f
JungTag <dbsdudxor121@naver.com>
2021-08-23 16:37:26 +0900

    feat: 고객센터 의견 보내기 API 추가

--- controllers/common.js
+++ controllers/common.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 const queryString = require('query-string');
-const url = require('url');
-const qs = require('query-string');
 const err = require('http-errors');
 
-const { param } = require('../utils/params');
+const { auth, param, parser, condition } = require('../utils/params');
 const { kakao } = require('../config');
+const tp = require('../utils/mailer');
+require('dotenv').config();
 const { encodeToken } = require('../utils/token');
 const { Users, User_sns_data, Shops, sequelize, Accounts } = require('../models');
 
@@ -238,6 +238,38 @@ const controller = {
                     nickname,
                 });
             }
+        } catch (e) {
+            next(e);
+        }
+    },
+    async sendOpinion({ user, body }, { pool }, next) {
+        try {
+            const user_no = auth(user, "user_no");
+            const title = param(body, "title");
+            const content = param(body, "content");
+
+            const sender = await Users.findOne({
+                where: {
+                    no: user_no,
+                    enabled: 1,
+                },
+                attributes: [
+                    'email',
+                    'name',
+                    'phone',
+                    'created_datetime'
+                ],
+                raw: true
+            });
+            
+            await tp.sendMail({
+                from: `[의견보내기] <${sender.email}>`,
+                to: `dbsdudxor121@naver.com`,
+                subject: `${title}`,
+                text: `${content}`,
+            });
+
+            next({ message: `등록되었습니다.` });
         } catch (e) {
             next(e);
         }
--- routes/common.js
+++ routes/common.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require("passport");
 const common = require('../controllers/common');
+const { checkUser } = require('../middlewares/auth');
 
 router.get('/', common.main);
 router.get('/ping', common.ping);
@@ -19,5 +20,7 @@ router.post(
 router.post('/oauth/callback/kakao', common.getKakaoCodeFromCallback);
 router.post('/test', common.main);
 router.post('/test2', common.main2);
+
+router.post('/cs/opinion', checkUser, common.sendOpinion);
 
 module.exports = router;
