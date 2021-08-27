34aea2c4577ee2fde863c12e0d275d20c93dbccf
JungTag <dbsdudxor121@naver.com>
2021-08-27 15:08:35 +0900

    remove: 카카오 로그인 API 제거

--- routes/common.js
+++ routes/common.js
@@ -4,23 +4,8 @@ const passport = require("passport");
 const common = require('../controllers/common');
 const { checkUser } = require('../middlewares/auth');
 
-router.get('/', common.main);
 router.get('/ping', common.ping);
-
-router.get('/kakao', passport.authenticate('kakao-login'));
-router.post(
-    '/kakao/oauth',
-    passport.authenticate('kakao-login', {
-        failureRedirect: 'http://localhost:8030/afterkakao',
-        successRedirect: 'http://localhost:8030/'
-    }),
-    // common.checkKakao
-);
-
 router.post('/oauth/callback/kakao', common.getKakaoCodeFromCallback);
-router.post('/test', common.main);
-router.post('/test2', common.main2);
-
 router.post('/cs/opinion', checkUser, common.sendOpinion);
 
 module.exports = router;
