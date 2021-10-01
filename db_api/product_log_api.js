82771a1247e028f6a667cdce037707390e07b0a3
jeongyeob97 <leejeongyeob97@gmail.com>
2021-10-01 12:59:12 +0900

    [#54] db_api 파일 생성 및 상품 상태 변화 로그 처리 함수 구현

---
+++ db_api/product_log_api.js
@@ -0,0 +1,21 @@
+const productLogModels = {
+    // 상품 상태 변화에 대한 로그 처리 함수
+    postLogProductStatusModels: async function(productNo, status, connection) {
+        try {
+            const [result] = await connection.query(`
+                INSERT INTO log_products_status(
+                    product_no,
+                    status
+                    )
+                VALUES(?,?)`,
+                    [
+                        productNo,
+                        status
+                    ]
+            );
+        } catch (e) {
+            throw e;
+        }
+    },
+}
+
