0418741ebed1ff5dcb5694bebedb4fe98b2c6bb9
jeongyeob97 <leejeongyeob97@gmail.com>
2021-10-01 15:13:32 +0900

    [#55] Reservations 상태 변경 로그 처리 함수 구현

--- db_api/reservation_log_api.js
+++ db_api/reservation_log_api.js
@@ -1,1 +1,21 @@
-const reservationLogApi
+const reservationLogApi = {
+    postLogReservationStatusModels: async function(reservationNo, status, connection) {
+        try {
+            const [result] = await connection.query(`
+                INSERT INTO log_reservations_status(
+                    reservation_no,
+                    status
+                    )
+                VALUES(?,?)`,
+                    [
+                        reservationNo,
+                        status
+                    ]
+            );
+        } catch (e) {
+            throw e;
+        }
+    },
+}
+
+module.exports = reservationLogApi;
